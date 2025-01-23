39d4e0bef340eca9bcdd3874bdc37b453f900246
remissg <maitijoydip888@gmail.com>
2025-01-23 21:02:04 +0530

    I

--- Lecture 3/For Loop/Sum_Of_EvenDigits.c
+++ Lecture 3/For Loop/Sum_Of_EvenDigits.c
@@ -7,9 +7,8 @@ int main (){
     int LastDigit = 0;
     while(n>0){
        LastDigit = n%10;
-       if(LastDigit % 2 == 0){
+       if(LastDigit % 2 == 0)
        sum = sum + LastDigit;
-       }
        n = n/10;
     }
     printf("The sum of even digits are %d",sum);
