612b5ad0edab397b117d4fbee786cb166961b5c7
remissg <maitijoydip888@gmail.com>
2025-02-03 12:12:18 +0530

    I

--- Lecture 4/Pattern Printing/0_1_Triangle.c
+++ Lecture 4/Pattern Printing/0_1_Triangle.c
@@ -3,9 +3,11 @@ int main (){
     int n;
     printf("Enter the number : ");
     scanf("%d",&n);
+
     for(int i=1;i<=n;i++){  
-        for(int j=1;j<=n+1-i;j++){  
-            printf("* ");
+        for(int j=1;j<=i;j++){  
+            if((i+j)%2==0)  printf("1 ");
+            else    printf("0 ");
         }
         printf("\n");   
     }
--- Lecture 4/Pattern Printing/Number_Triangle.c
+++ Lecture 4/Pattern Printing/Number_Triangle.c
@@ -3,9 +3,10 @@ int main (){
     int n;
     printf("Enter the number : ");
     scanf("%d",&n);
+
     for(int i=1;i<=n;i++){  
-        for(int j=1;j<=n+1-i;j++){  
-            printf("* ");
+        for(int j=1;j<=i;j++){  
+            printf("%d ",j);
         }
         printf("\n");   
     }
